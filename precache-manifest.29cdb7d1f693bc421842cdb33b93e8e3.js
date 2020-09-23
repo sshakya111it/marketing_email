6f195ec1b125c6e6d74b6a329896b931658323fc
Samrat Shakya <sshakya.111it@gmail.com>
2020-09-23 15:14:07 +1000

    Updates

---
+++ precache-manifest.29cdb7d1f693bc421842cdb33b93e8e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80909ee71b22a5f3968aa6e6ff2bd59",
+    "url": "/marketing_email/index.html"
+  },
+  {
+    "revision": "ac7273270287043a9e87",
+    "url": "/marketing_email/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c56ad2ef209ad5776407",
+    "url": "/marketing_email/static/css/main.7e382ac3.chunk.css"
+  },
+  {
+    "revision": "ac7273270287043a9e87",
+    "url": "/marketing_email/static/js/2.bcce87cc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/marketing_email/static/js/2.bcce87cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56ad2ef209ad5776407",
+    "url": "/marketing_email/static/js/main.6cc091ba.chunk.js"
+  },
+  {
+    "revision": "dece6a7c38ab2fdf86eb",
+    "url": "/marketing_email/static/js/runtime-main.47055ebf.js"
+  }
+]);
